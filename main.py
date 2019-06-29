65a7b939b31bf0112e3137e76692370939f1dcd6
riadibadulla <riadibadulla@gmail.com>
2019-06-29 18:37:11 +0100

    OOP Fully constructed

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import cv2
 import numpy as np
 import time 
-import ImageStitcher
+from ImageStitcher import ImageStitcher
 
-START_TIME = time.time()
+class main:
 
-img1 = cv2.imread('/cs/home/ri31/project-scripts/canvasWithPictures/images/file.jpg')
-img2 = cv2.imread('/cs/home/ri31/project-scripts/canvasWithPictures/images/file1.jpg')  
+    def mosaic2Images(img1,img2):
+        mosaic = ImageStitcher(img1,img2,False)
+        mosaic.mosaicImages()
+        mosaic.drawImage(mosaic.BestX,mosaic.BestY)
 
-mosaic = ImageStitcher(img1,img2,True)
-mosaic.mosaicImages()
-mosaic.drawImage(mosaic.BestX,mosaic.BestY)
-
-END_TIME = time.time()
-print("Time taken: ", format(END_TIME - START_TIME), " seconds")
+    if __name__=='__main__':
+        START_TIME = time.time()
+        img1 = cv2.imread('/cs/home/ri31/project-scripts/images/Map1.png')
+        img2 = cv2.imread('/cs/home/ri31/project-scripts/images/Map2.png')  
+        mosaic2Images(img1,img2)
+        END_TIME = time.time()
+        print("Time taken: ", format(END_TIME - START_TIME), " seconds")
