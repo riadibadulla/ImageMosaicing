rotation added, with erros to bi fixed

ImageStitcher.py
@@ -21,6 +21,7 @@ class ImageStitcher:
             self.img2 = cv2.resize(self.img2,None,fx=0.1,fy=0.1)
         self.h1, self.w1 = self.img1.shape[:2]
         self.h2, self.w2 = self.img2.shape[:2]
+
         print("H1: ",self.h1,"  W1: ",self.w1,"\nH2: ",self.h2,"  W2: ",self.w2)
         
 
@@ -44,14 +45,18 @@ class ImageStitcher:
         return {'x5':x5,'y5':y5,'x6':x6,'y6':y6}
 
     def calculateLoss(self,SHIFT):
-        SHIFT_X,SHIFT_Y = SHIFT
-        SHIFT_X, SHIFT_Y = int(SHIFT_X*(self.w1+self.w2)),int(SHIFT_Y*(self.h1+self.h2))
-        print("SHIFT_X: ",SHIFT_X,"    SHIFT_Y: ",SHIFT_Y)
+        SHIFT_X,SHIFT_Y, thetha = SHIFT
+        SHIFT_X, SHIFT_Y, thetha = int(SHIFT_X*(self.w1+self.w2)),int(SHIFT_Y*(self.h1+self.h2)),int(thetha*360)
+        print("SHIFT_X: ",SHIFT_X,"    SHIFT_Y: ",SHIFT_Y,"    Angle: ",thetha)
+        #self.drawImage(SHIFT_X,SHIFT_Y)
         if (SHIFT_X>=self.w1+self.w2 or SHIFT_Y>=self.h1+self.h2 or SHIFT_Y<1 or SHIFT_X<1):
             return 255*3*self.w1*self.h1*self.h2*self.w2
-        canvas = np.zeros((self.h1*2+self.h2,self.w1*2+self.w2,3), dtype=np.uint8)
+        
+        canvas = self.rotateImage(thetha)
         canvas[:self.h1,:self.w1,:3] = self.img1
-        canvas[self.h1:self.h2+self.h1, self.w1:self.w1+self.w2,:3] = self.img2
+
+        # cv2.imshow('image',canvas)
+        # cv2.waitKey(500)
 
         coor = self.getIntersectionCoordinates(self.h1,self.w1,self.h2,self.w2,SHIFT_X,SHIFT_Y)
         image1 = canvas[coor['y6']-SHIFT_Y:coor['y5']-SHIFT_Y,coor['x5']-SHIFT_X:coor['x6']-SHIFT_X,:3]
@@ -76,21 +81,21 @@ class ImageStitcher:
         h, w = canvas.shape[:2]
         M = np.float32([[a,b,(1-a)*w/2-b*h/2],[-b,a,b*w/2+(1-a)*h/2],[0,0,1]])
         canvas = cv2.warpPerspective(canvas,M,(w,h))
-        canvas[:self.h1,:self.w1,:3] = self.img1
+        # canvas[:self.h1,:self.w1,:3] = self.img1
         return canvas
 
 
     def mosaicImages(self,n):
         savedParameters = [[],[]]
-        SHIFT_X = 1
-        SHIFT_Y = 1
         intitial_cors_x = np.linspace(0.1,1,n,False)
         intitial_cors_y = np.linspace(0.1,1,n,False)
-        for i in range(n*n):
-            x = intitial_cors_x[i%n]
-            y = intitial_cors_y[int(i/n)]
+        initial_param_thetha = np.linspace(0,1,n,False)
+        for i in range(n*n*n):
+            thetha = initial_param_thetha[int(i/n)]
+            x = intitial_cors_x[i%n%n]
+            y = intitial_cors_y[int(i%n/n)]
             print("Initial Values: ",x,"  ",y)
-            x0 = [x,y]
+            x0 = [x,y,thetha]
             res = minimize(self.calculateLoss,x0, method = 'nelder-mead', options={'disp':True})
             savedParameters[0].append(res.fun)
             savedParameters[1].append(res.x)
@@ -102,4 +107,5 @@ class ImageStitcher:
         print(savedParameters[0][minimumErrorIndex])
         print(savedParameters[1][minimumErrorIndex][0]*(self.w1+self.w2))
         print(savedParameters[1][minimumErrorIndex][1]*(self.w1+self.w2))
+        print(savedParameters[1][minimumErrorIndex][2]*360)
         self.BestX, self.BestY = int(savedParameters[1][minimumErrorIndex][0]*(self.w1+self.w2)),int(savedParameters[1][minimumErrorIndex][1]*(self.h1+self.h2))