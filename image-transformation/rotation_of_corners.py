a676c11e8176c349499dab199025c2d7f44fa8b1
riadibadulla <riadibadulla@gmail.com>
2019-07-12 16:56:50 +0100

    Algorith of finding coordinates without rotation is developed in rotation_of_corners

--- image-transformation/rotation_of_corners.py
+++ image-transformation/rotation_of_corners.py
@@ -1,62 +1,65 @@
-import cv2
 import numpy as np
 import math
-def rotateImage(angleInDegrees,canvas):
-    thetha = angleInDegrees * math.pi/180
-    a = math.cos(thetha)
-    b = math.sin(thetha)
-    h, w = canvas.shape[:2]
-    M = np.float32([[a,b,(1-a)*w/2-b*h/2],[-b,a,b*w/2+(1-a)*h/2],[0,0,1]])
-    canvas = cv2.warpPerspective(canvas,M,(w,h))
-    return canvas
+from shapely.geometry import Polygon
+from shapely.geometry import Point
+import time
 
-canvas = np.zeros((50,50,3), dtype=np.uint8)
-canvas[10][10]=[255,255,255]
-canvas[10][20]=[255,255,255]
-canvas[20][10]=[255,255,255]
-canvas[20][20]=[255,255,255]
-canvas2 = rotateImage(45,canvas)
-cv2.imshow('image',canvas)
-cv2.waitKey(0)
-cv2.imshow('image',canvas2)
-cv2.waitKey(0)
-
-def rotateTranslateCoordinates(X,Y,centreX,centreY):
+def rotateTranslateCoordinates(coor,centreX,centreY,angle):
+    X,Y = coor
     tempX = X - centreX
     tempY = Y - centreY
-    rotatedX = tempX*math.cos(45 * math.pi/180) - tempY*math.sin(45 * math.pi/180)
-    rotatedY = tempX*math.sin(45 * math.pi/180) + tempY*math.cos(45 * math.pi/180)
+    rotatedX = tempX*math.cos(angle * math.pi/180) - tempY*math.sin(angle * math.pi/180)
+    rotatedY = tempX*math.sin(angle * math.pi/180) + tempY*math.cos(angle * math.pi/180)
     x = rotatedX + centreX
     y = rotatedY + centreY
     return x,y
 
-canvas3 = np.zeros((50,50,3), dtype=np.uint8)
-x,y = RotateTranslateCoordinates(10,10,15,15)
-canvas3[int(x)][int(y)]=[255,255,255]
+def getCentreOfPolygon(coordinates):
+    p = Polygon(coordinates)
+    bound = p.bounds
+    centreX = (bound[2] + bound[0])/2
+    centreY = (bound[3] + bound[1])/2
+    return centreX,centreY
 
-tempX = 20 - 15
-tempY = 10 - 15
-rotatedX = tempX*math.cos(45 * math.pi/180) - tempY*math.sin(45 * math.pi/180)
-rotatedY = tempX*math.sin(45 * math.pi/180) + tempY*math.cos(45 * math.pi/180)
-x = rotatedX + 15
-y = rotatedY + 15
-canvas3[int(x)][int(y)]=[255,255,255]
+def rotateCornersOfImage(rectangle, centre,angle):
+    newRectangle = []
+    for coor in rectangle:
+        newCorner = rotateTranslateCoordinates(coor,centre[0],centre[1],angle)
+        newRectangle.append(newCorner)
+    return newRectangle
 
-tempX = 10 - 15
-tempY = 20 - 15
-rotatedX = tempX*math.cos(45 * math.pi/180) - tempY*math.sin(45 * math.pi/180)
-rotatedY = tempX*math.sin(45 * math.pi/180) + tempY*math.cos(45 * math.pi/180)
-x = rotatedX + 15
-y = rotatedY + 15
-canvas3[int(x)][int(y)]=[255,255,255]
+def getCoordinatesInPolygon(intersection):
+    bounds = intersection.bounds
+    coordinatesInPolygon = []
+    for y in range(int(bounds[1]),int(bounds[3])):
+        for x in range(int(bounds[0]),int(bounds[2])):
+            if (intersection.contains(Point(x,y))):
+                coordinatesInPolygon.append((x,y))
+    return coordinatesInPolygon
 
-tempX = 20 - 15
-tempY = 20 - 15
-rotatedX = tempX*math.cos(45 * math.pi/180) - tempY*math.sin(45 * math.pi/180)
-rotatedY = tempX*math.sin(45 * math.pi/180) + tempY*math.cos(45 * math.pi/180)
-x = rotatedX + 15
-y = rotatedY + 15
-canvas3[int(x)][int(y)]=[255,255,255]
+def getCoorOfIntersectionInInitImg(coordinatesInPolygon,centreOfRectangle,angle):
+    listOfInitialCoor = []
+    for coor in coordinatesInPolygon:
+        listOfInitialCoor.append(rotateTranslateCoordinates(coor,centreOfRectangle[0],centreOfRectangle[1],angle))
+    return listOfInitialCoor
 
-cv2.imshow('image',canvas3)
-cv2.waitKey(0)
+
+START_TIME = time.time()
+
+rectangle1 = [(0,0),(0,10),(10,10),(10,0)]
+rectangle2 = [(10,0),(10,10),(20,10),(20,0)]
+centreOfRectangle1 = getCentreOfPolygon(rectangle1)
+centreOfRectangle2 = getCentreOfPolygon(rectangle2)
+rectangle1 = rotateCornersOfImage(rectangle1,centreOfRectangle1,45)
+rectangle2 = rotateCornersOfImage(rectangle2,centreOfRectangle2,45)
+
+p1 = Polygon(rectangle1)
+p2 = Polygon(rectangle2)
+intersection = p1.intersection(p2)
+coordinatesInPolygon = getCoordinatesInPolygon(intersection)
+initialCoord1 = getCoorOfIntersectionInInitImg(coordinatesInPolygon,centreOfRectangle1,-45)
+print(initialCoord1)
+
+END_Time = time.time()
+print(END_Time-START_TIME)
+
