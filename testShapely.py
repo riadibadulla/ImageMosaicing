Rotation is fixed

testShapely.py
@@ -2,11 +2,10 @@ from shapely.geometry import Polygon
 from shapely.geometry import Point
 from shapely.affinity import affine_transform
 from shapely.geometry import LineString
+from shapely.wkt import loads
+from shapely.wkt import dumps
 import matplotlib.pyplot as plt
 import numpy as np
 
-polyg = Polygon([(0,0),(0,10),(10,10),(10,0)])
-int_coords = lambda x: np.array(x).round().astype(np.int32)
-interiors = [int_coords(pi.coords) for poly in [polyg]
-                 for pi in poly.interiors]
-print(interiors)
+polyg = Polygon([(0,0),(0,10.23),(10.12,10.1),(10,0.4)])
+print(loads(dumps(polyg, rounding_precision=0)))