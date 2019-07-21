6195b1f24ded93f65d24930412a8f069d55467e4
riadibadulla <riadibadulla@gmail.com>
2019-07-21 14:57:27 +0100

    minor changes in UI

--- ImageStitcher.py
+++ ImageStitcher.py
@@ -44,7 +44,7 @@ class ImageStitcher:
         SHIFT_X, SHIFT_Y, thetha = int(SHIFT_X*(self.w1+self.w2)),int(SHIFT_Y*(self.h1+self.h2)),int(thetha*360)
         # SHIFT_X, SHIFT_Y, thetha = int(SHIFT_X),int(SHIFT_Y),int(thetha)
         # print("\rSHIFT_X: ",SHIFT_X,"    SHIFT_Y: ",SHIFT_Y,"    Angle: ",thetha)
-        sys.stdout.write("\r    SHIFT_X:{0}    ,SHIFT_Y:{1}    ,Angle:{2}         ☚||||".format(SHIFT_X,SHIFT_Y,thetha))
+        sys.stdout.write("\r    SHIFT_X:{0},    SHIFT_Y:{1},    Angle:{2}         ☚||||".format(SHIFT_X,SHIFT_Y,thetha))
         if (SHIFT_X>=self.w1+self.img2_canvas_size-self.img2_canvas_size*0.05 or SHIFT_Y>=self.h1+self.img2_canvas_size-self.img2_canvas_size*0.05 or SHIFT_Y<self.img2_canvas_size*0.05 or SHIFT_X<self.img2_canvas_size*0.05):
             return 255*3*self.w1*self.h1*self.h2*self.w2
         self.coor_system.set_rectangles(self.getCornersOfImages((SHIFT_X,SHIFT_Y, 360-thetha)))
@@ -86,7 +86,7 @@ class ImageStitcher:
         self.coor_system = CoordinateSystem((len(self.canvas[0])/2,len(self.canvas)/2))
 
     def mosaicImages(self,n):
-        print("\n\n\n\n\n\n")
+        print("\n\n\n")
         self.set_canvas()
         savedParameters = [[],[]]
         for i in range(n):
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import time 
 from ImageStitcher import ImageStitcher
+import os
 
 class main:
 
@@ -10,10 +11,15 @@ class main:
         mosaic.mosaicImages(20)
         mosaic.drawImage(mosaic.BestX,mosaic.BestY,360-mosaic.Best_Rotate,0)
 
+    def clearScreen():
+        os.system('clear')
+
     if __name__=='__main__':
         START_TIME = time.time()
+        clearScreen()
         img1 = cv2.imread('images/Map1-rotate.png')
         img2 = cv2.imread('images/Map2.png')  
+        print("Starting the algorithm\n\n")
         mosaic2Images(img1,img2)
         END_TIME = time.time()
         print("Time taken: ", format(END_TIME - START_TIME), " seconds")
